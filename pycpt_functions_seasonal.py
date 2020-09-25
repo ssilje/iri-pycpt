ab0be6a60138013c371717bae0ccdea73b0e88c5
Ángel G. Muñoz <agmunoz@iri.columbia.edu>
2020-09-25 09:56:41 -0400

    Added T2M mean, max, min

--- pycpt_functions_seasonal.py
+++ pycpt_functions_seasonal.py
@@ -1,4 +1,4 @@
-#This is PyCPT_functions_seasonal.py (version1.8) -- 7 Aug 2020
+#This is PyCPT_functions_seasonal.py (version1.8) -- 25 Sep 2020
 #Authors: AG Muñoz (agmunoz@iri.columbia.edu) and Andrew W. Robertson (awr@iri.columbia.edu)
 #Notes: be sure it matches version of PyCPT
 #Requires: CPTv16.5.2+
